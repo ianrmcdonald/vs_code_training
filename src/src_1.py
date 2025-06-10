1375705a841925d0883b84e8ea87601e7bb80873
Ian McDonald <ianrmcdonald@gmail.com>
2025-06-10 11:35:49 -0700

    First commit from Mac Mini

--- src/src_1.py
+++ src/src_1.py
@@ -1,1 +1,12 @@
 import pandas as pd
+df = pd.DataFrame({'num_legs': [2, 4, 8, 0],
+                   'num_wings': [2, 0, 0, 0],
+                   'num_specimen_seen': [10, 2, 1, 8]},
+                  index=['falcon', 'dog', 'spider', 'fish'])
+print(df)
+
+import numpy as np
+
+mu, sigma = 5, 1 # mean and standard deviation
+s = np.random.normal(mu, sigma, 10)
+print(min(s))
